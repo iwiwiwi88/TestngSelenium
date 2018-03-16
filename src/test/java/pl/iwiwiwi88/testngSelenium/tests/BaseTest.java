287695e5504217f906ce80c0a22b8ff0fbca8ca0
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-03-16 11:52:48 +0100

    BaseTest added - setup and teardown works fine

---
+++ src/test/java/pl/iwiwiwi88/testngSelenium/tests/BaseTest.java
@@ -0,0 +1,30 @@
+package pl.iwiwiwi88.testngSelenium.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    @Parameters({"browser"})
+    public void setup(String browser) {
+        if (browser.equals("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "/Users/iwonasasak/Documents/selenium/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser.equals("Firefox")) {
+            System.setProperty("webdriver.gecko.driver", "/Users/iwonasasak/Documents/selenium/geckodriver");
+            driver = new FirefoxDriver();
+        }
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
+}
--- src/test/java/pl/iwiwiwi88/testngSelenium/tests/TimVRoomTest.java
+++ src/test/java/pl/iwiwiwi88/testngSelenium/tests/TimVRoomTest.java
@@ -1,4 +1,37 @@
 package pl.iwiwiwi88.testngSelenium.tests;
 
-public class TimVRoomTest {
+    /* 1. Grab page title and place title text in answer slot #1
+       2. Fill out name section of form to be Kilgore Trout
+       3. Set occupation on form to Sci-Fi Author
+       4. Count number of blue_boxes on page after form and enter into answer box #4
+       5. Click link that says 'click me'
+       6. Find red box on its page find class applied to it, and enter into answer box #6
+       7. Run JavaScript function as: ran_this_js_function() from your Selenium script
+       8. Run JavaScript function as: got_return_from_js_function() from your Selenium script, take returned value and place it in answer slot #8
+       9. Mark radio button on form for written book? to Yes
+       10. Get the text from the Red Box and place it in answer slot #10
+       11. Which box is on top? orange or green -- place correct color in answer slot #11
+       12. Set browser width to 850 and height to 650
+       13. Type into answer slot 13 yes or no depending on whether item by id of ishere is on the page
+       14. Type into answer slot 14 yes or no depending on whether item with id of purplebox is visible
+       15. Waiting game: click the link with link text of 'click then wait' a random wait will occur (up to 10 seconds) and then a new link will get added with link text of 'click after wait' - click this new link within 500 ms of it appearing to pass this test
+       16. Click OK on the confirm after completing task 15
+       17. Click the submit button on the form
+        */
+
+import org.testng.annotations.Test;
+
+public class TimVRoomTest extends BaseTest {
+
+    @Test
+    public void goThroughTheSteps() {
+        driver.get("http://timvroom.com/selenium/playground/");
+        driver.manage().window().maximize();
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+
+        }
+    }
+
 }
